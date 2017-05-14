e49fdaa3654cbd4e631bdf5fd958b26cfbb49fd7
Aulina <aulinarj@gmail.com>
2017-05-14 16:47:00 +0700

    Moviews with design

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl505/moviews/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl505/moviews/MainActivity.java
@@ -1,9 +1,7 @@
 package id.sch.smktelkom_mlg.privateassignment.xirpl505.moviews;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,7 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import id.sch.smktelkom_mlg.privateassignment.xirpl505.moviews.adapter.ComingSoonAdapter;
 import id.sch.smktelkom_mlg.privateassignment.xirpl505.moviews.adapter.SourceAdapter;
@@ -27,15 +24,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -91,8 +79,6 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
         changePage(id);
         return true;
-
-
     }
 
     private void changePage(int id) {
